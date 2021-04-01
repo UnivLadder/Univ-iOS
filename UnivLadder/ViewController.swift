dd2afafbc29a09eb7ea2bbdd466f07bb7f5fd9dc
Ahyeon Gil <ahyeonlog@gmail.com>
2021-04-01 20:53:50 +0900

    Initial Commit

---
+++ UnivLadder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnivLadder
+//
+//  Created by Ahyeonway on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
