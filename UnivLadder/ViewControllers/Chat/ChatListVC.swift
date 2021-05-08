f03821ac472777a3fb9742bf77d586bad3d02018
Ahyeon Gil <ahyeonlog@gmail.com>
2021-05-09 03:50:55 +0900

    feat : 프로젝트 구조 세팅

---
+++ UnivLadder/ViewControllers/Chat/ChatListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatListVC.swift
+//  UnivLadder
+//
+//  Created by Ahyeonway on 2021/05/09.
+//
+
+import UIKit
+
+class ChatListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
