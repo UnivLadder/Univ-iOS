521fd78d7136a222317f097465208be4f881ddcf
Ahyeon Gil <ahyeonlog@gmail.com>
2021-05-09 23:58:23 +0900

    feat : chat bubble 스토리보드 구현

---
+++ UnivLadder/ViewControllers/Chat/ChatBubbleCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatBubbleCell.swift
+//  UnivLadder
+//
+//  Created by Ahyeonway on 2021/05/09.
+//
+
+import UIKit
+
+class ChatBubbleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
