743e5abc507cea7a34460d162c4aed04c5024612
SungjiCho <whtjdwl34@naver.com>
2021-05-14 18:01:42 +0900

    20210514

--- UnivLadder/SupportingFiles/AppDelegate.swift
+++ UnivLadder/SupportingFiles/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        sleep(100)
+    }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
