ea02ea1575c478ec12b27dfec8c4c47406a9fe13
lxxyeon <lxxyeon@gmail.com>
2021-12-06 20:59:50 +0900

    Account 화면 개발

---
+++ UnivLadder/ViewControllers/Accounts/AccountsRegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountsRegisterViewController.swift
+//  UnivLadder
+//
+//  Created by leeyeon2 on 2021/12/06.
+//
+
+import UIKit
+
+class AccountsRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
