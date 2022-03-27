e4f6f1f15021d709c877fcbc335a1cc083b9ed08
lxxyeon <lxxyeon@gmail.com>
2022-03-28 08:39:01 +0900

    no message

---
+++ UnivLadder/Cells/MyPage/PageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PageCollectionViewCell.swift
+//  UnivLadder
+//
+//  Created by leeyeon2 on 2022/03/27.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var subjectTitle: UILabel!
+}
--- UnivLadder/ViewControllers/MyPage/SubjectModifyViewController.swift
+++ UnivLadder/ViewControllers/MyPage/SubjectModifyViewController.swift
@@ -9,29 +9,41 @@ import UIKit
 
 class SubjectModifyViewController: UIViewController {
     
+    // 서치바
     @IBOutlet weak var searchBtn: UIButton!
     let searchBar = UISearchBar()
     
     // 카테고리바
     @IBOutlet weak var subjecCategoryCollectionView: UICollectionView!
     
-//    @IBOutlet weak var pageCollectionView: UICollectionView!
+    // 페이징
+    @IBOutlet weak var pageCollectionView: UICollectionView!
+    
+    // 하이라이트 뷰
+    @IBOutlet weak var highlightView: UIView!
+    
+    // Data - 과목은 서버에서 받아와
+    private let subjectCategoryList = ["전체", "교과목", "수시/논술", "입시/경시대회", "외국어" ,"외국어 시험", "미술", "음악", "악기", "국악", "댄스", "IT/컴퓨터", "디자인", "취업 준비", "스포츠", "패션/뷰티", "사진/영상", "연기/공연/영화", "요리/커피"]
+    
+    private let subjectList: [Int: [String]] = [1 : ["전체", "교과목"],
+                                                2 : ["전체", "교과목"]]
     
     
-    var subjecCategoryList = ["전체","교과목", "수시/논술", "입시/경시대회", "외국어" ,"외국어 시험", "미술", "음악", "악기", "국악", "댄스", "IT/컴퓨터", "디자인", "취업 준비", "스포츠", "패션/뷰티", "사진/영상", "연기/공연/영화", "요리/커피"]
+    var constraints: [NSLayoutConstraint] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = self.rightButton
-        //        self.configureLayout()
         
+        self.configureLayout()
         self.collectionViewLayout()
+        highlightView.backgroundColor = .darkGray
     }
     
     private func collectionViewLayout() {
         //카테고리바
         let categoryLayout = UICollectionViewFlowLayout()
-        categoryLayout.itemSize = CGSize(width: 100, height: 50)
+        categoryLayout.itemSize = CGSize(width: 70, height: 50)
         categoryLayout.minimumLineSpacing = 10
         categoryLayout.scrollDirection = .horizontal
         subjecCategoryCollectionView.frame = .zero
@@ -40,42 +52,40 @@ class SubjectModifyViewController: UIViewController {
         subjecCategoryCollectionView.showsHorizontalScrollIndicator = false
         
         //페이징바
-//        let pageLayout = UICollectionViewFlowLayout()
-//        pageLayout.itemSize = CGSize(width: 390, height: 600)
-//        pageLayout.minimumLineSpacing = 0
-//        pageLayout.scrollDirection = .horizontal
-//        pageCollectionView.frame = .zero
-//        pageCollectionView.collectionViewLayout = pageLayout
-//        pageCollectionView.decelerationRate = .fast
-//        pageCollectionView.showsHorizontalScrollIndicator = false
+        let pageLayout = UICollectionViewFlowLayout()
+        pageLayout.itemSize = CGSize(width: 390, height: 600)
+        pageLayout.minimumLineSpacing = 0
+        pageLayout.scrollDirection = .horizontal
+        pageCollectionView.frame = .zero
+        pageCollectionView.collectionViewLayout = pageLayout
+        pageCollectionView.decelerationRate = .fast
+        pageCollectionView.showsHorizontalScrollIndicator = false
     }
     
-    
-    
     private func configureLayout() {
-        
         subjecCategoryCollectionView.translatesAutoresizingMaskIntoConstraints = false
         subjecCategoryCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         subjecCategoryCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         subjecCategoryCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         subjecCategoryCollectionView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        //        highlightView.translatesAutoresizingMaskIntoConstraints = false
-        //        constraints = [
-        //            highlightView.topAnchor.constraint(equalTo: tapBarCollectionView.bottomAnchor),
-        //            highlightView.bottomAnchor.constraint(equalTo: pageCollectionView.topAnchor),
-        //            highlightView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
-        //            highlightView.heightAnchor.constraint(equalToConstant: 1),
-        //            highlightView.widthAnchor.constraint(equalToConstant: 80)
-        //        ]
-        //        NSLayoutConstraint.activate(constraints)
-        //
-        //        pageCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        //        pageCollectionView.topAnchor.constraint(equalTo: highlightView.bottomAnchor).isActive = true
-        //        pageCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        //        pageCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        //        pageCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        highlightView.translatesAutoresizingMaskIntoConstraints = false
+        constraints = [
+            highlightView.topAnchor.constraint(equalTo: subjecCategoryCollectionView.bottomAnchor),
+            //            highlightView.bottomAnchor.constraint(equalTo: pageCollectionView.topAnchor),
+            highlightView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            highlightView.heightAnchor.constraint(equalToConstant: 1),
+            highlightView.widthAnchor.constraint(equalToConstant: 80)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        pageCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        pageCollectionView.topAnchor.constraint(equalTo: highlightView.bottomAnchor).isActive = true
+        pageCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        pageCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        pageCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
+    
     lazy var rightButton: UIBarButtonItem = {
         let button = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(buttonPressed(_:)))
         button.tag = 1
@@ -100,29 +110,29 @@ extension SubjectModifyViewController: UICollectionViewDelegate, UICollectionVie
         if collectionView == subjecCategoryCollectionView {
             guard let cell = subjecCategoryCollectionView.cellForItem(at: indexPath) as? SubjectCategoryCollectionViewCell else { return }
             
-            //            NSLayoutConstraint.deactivate(constraints)
-            //            highlightView.translatesAutoresizingMaskIntoConstraints = false
-            //            constraints = [
-            //                highlightView.topAnchor.constraint(equalTo: tapBarCollectionView.bottomAnchor),
-            //                highlightView.bottomAnchor.constraint(equalTo: pageCollectionView.topAnchor),
-            //                highlightView.heightAnchor.constraint(equalToConstant: 1),
-            //                highlightView.leadingAnchor.constraint(equalTo: cell.leadingAnchor),
-            //                highlightView.trailingAnchor.constraint(equalTo: cell.trailingAnchor)
-            //            ]
-            //            NSLayoutConstraint.activate(constraints)
+            NSLayoutConstraint.deactivate(constraints)
+            highlightView.translatesAutoresizingMaskIntoConstraints = false
+            constraints = [
+                highlightView.topAnchor.constraint(equalTo: subjecCategoryCollectionView.bottomAnchor),
+                //                highlightView.bottomAnchor.constraint(equalTo: pageCollectionView.topAnchor),
+                highlightView.heightAnchor.constraint(equalToConstant: 1),
+                highlightView.leadingAnchor.constraint(equalTo: cell.leadingAnchor),
+                highlightView.trailingAnchor.constraint(equalTo: cell.trailingAnchor)
+            ]
+            NSLayoutConstraint.activate(constraints)
             
             UIView.animate(withDuration: 0.3) {
                 self.view.layoutIfNeeded()
             }
             
-            //            pageCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            pageCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         if collectionView == subjecCategoryCollectionView{
-            return subjecCategoryList.count
+            return subjectCategoryList.count
         }else{
             return 1
         }
@@ -131,18 +141,28 @@ extension SubjectModifyViewController: UICollectionViewDelegate, UICollectionVie
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SubjectCategoryCell", for: indexPath) as! SubjectCategoryCollectionViewCell
-        cell.backgroundColor = .white
-        cell.subjectCategoryTitleLabel.text = subjecCategoryList[indexPath.row]
         
-        //사용자의 기존 선택한 과목 보여주기
-        //        if {
-        //            cell.isSelected = true
-        //        }
-        
-        return cell
+        if collectionView == subjecCategoryCollectionView {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SubjectCategoryCell", for: indexPath) as! SubjectCategoryCollectionViewCell
+            cell.backgroundColor = .white
+            cell.subjectCategoryTitleLabel.text = subjectCategoryList[indexPath.row]
+            
+            //            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TabCollectionViewCell.identifier, for: indexPath) as? TabCollectionViewCell else { return UICollectionViewCell() }
+            //            cell.configureCell(tapName[indexPath.item])
+            
+            return cell
+        } else if collectionView == pageCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pageCell", for: indexPath) as? PageCollectionViewCell else { return UICollectionViewCell() }
+            
+//            cell.subjectTitle.text = subjectList[indexPath.row]
+            //            cell.configureCell(color: pageBackgroundColor[indexPath.item])
+            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
     }
-    
 }
 
 
